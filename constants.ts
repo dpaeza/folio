f4b5bbf1ad532b37184aec49d2eb708a8f7807d6
dpaeza10 <daniela.paez@inchcape.com>
2025-05-13 11:15:33 -0500

    Update site URL in METADATA to reflect the correct domain

--- constants.ts
+++ constants.ts
@@ -8,7 +8,7 @@ export const METADATA = {
   title: "Daniela Paez",
   description:
     "I bridge the gap between design and development. I take responsibility to craft an aesthetic user experience using modern frontend architecture.",
-  siteUrl: "https://ayushsingh.net/",
+  siteUrl: "https://danielapaez.dev/",
 };
 
 export const MENULINKS = [
